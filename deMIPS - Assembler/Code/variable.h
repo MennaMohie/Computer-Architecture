70dfad06e0554e32b512dc52c56c1f18427828c1
Mirna <MirnaMuhammad98@gmail.com>
2019-04-02 20:46:27 +0200

    Update variable.h

--- deMIPS - Assembler/Code/variable.h
+++ deMIPS - Assembler/Code/variable.h
@@ -1,1 +1,33 @@
+#pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+vector<string>split(string, string);
+string to_binary_string(int);
+string to_binary_string(string);
+
+class variable
+{
+public:
+	string type;
+	string label;
+	int third_argument;
+	int address;
+
+	variable();
+
+	variable(string line);
+
+	string variable_conversion();
+};
